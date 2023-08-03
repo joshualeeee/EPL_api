53b4baab878b7104a2d8a56442650d67fa2d9930
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-08-03 09:13:15 -1000

    chore: added cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const connection = require("./connection");
 const standingsRouter = require("./routes/standings");
 
 const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:1234",
+  };
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
