5bd2dd733b9200d45bd772522133559c4128473d
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2023-08-03 09:13:38 -1000

    feat: added testing suite

---
+++ test.js
@@ -0,0 +1,60 @@
+const request = require("supertest");
+const app = require("./index"); // Replace "./app" with the path to your main app file
+
+describe("GET /standings", () => {
+  it("should return 200 with valid query parameters", (done) => {
+    request(app)
+      .get("/standings")
+      .query({
+        season: "2013-14",
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        const standings = res.body;
+        standings.forEach((standing) => {
+          // Assert that each standing has the correct season
+          if (standing.Season !== "2013-14") {
+            throw new Error(`Invalid season in response: ${standing.Season}`);
+          }
+        });
+        done();
+      });
+  });
+
+  it("should return 400 if season format is invalid", (done) => {
+    request(app)
+      .get("/standings")
+      .query({
+        season: "invalid-season-format",
+      })
+      .expect(400, done);
+  });
+
+  it("should return 400 if searchTeam contains numbers", (done) => {
+    request(app)
+      .get("/standings")
+      .query({
+        searchTeam: "TeamWithNumbers123",
+      })
+      .expect(400, done);
+  });
+
+  it("should return 400 if wins[gte] is a negative value", (done) => {
+    request(app)
+      .get("/standings")
+      .query({
+        wins_gte: -1,
+      })
+      .expect(400, done);
+  });
+
+  it("should return 400 if ordering is invalid", (done) => {
+    request(app)
+      .get("/standings")
+      .query({
+        ordering: "invalid-ordering",
+      })
+      .expect(400, done);
+  });
+});
